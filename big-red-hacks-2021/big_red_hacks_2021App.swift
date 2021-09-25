f1059d5d24fdda943b24b4e6310e5748a5cb442b
Robin Lin <zl755@cornell.edu>
2021-09-24 18:33:43 -0700

    Initial Commit

---
+++ big-red-hacks-2021/big_red_hacks_2021App.swift
@@ -0,0 +1,17 @@
+//
+//  big_red_hacks_2021App.swift
+//  big-red-hacks-2021
+//
+//  Created by Robin Lin on 2021-09-24.
+//
+
+import SwiftUI
+
+@main
+struct big_red_hacks_2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
